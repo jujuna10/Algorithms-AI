ff11509847d4e0bb57e4fc2102da9fd725ca50aa
jujuna10 <jujunaniko@gmail.com>
2024-10-09 17:41:34 +0400

    commit

---
+++ char_countt/main.c
@@ -0,0 +1,11 @@
+ #include <stdio.h>
+ /* count characters in input' lst version */
+int main( )
+{
+     long nc;
+     for (nc = 0; getchar() != EOF; ++nc)
+            ;
+     printf ("\nsimboloebis raodenoba: %ld\n\n", nc);
+     
+     return 0;
+}
